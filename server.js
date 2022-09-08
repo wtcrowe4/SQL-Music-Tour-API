61ca762a9dec38ce989b1c97984da3076a9c780d
Thomas Crowe <wcrowe@g.clemson.edu>
2022-09-07 21:12:20 -0400

    Set up connection to database.

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: process.env.PG_PASSWORD,
+})
+try {
+    sequelize.authenticate()
+    console.log(`Connection with postgres database established successfully.`)
+} catch (err) {
+    console.log(`Unable to connect to the database: ${err}`)
+}
 
 // ROOT
 app.get('/', (req, res) => {
